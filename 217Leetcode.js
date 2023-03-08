0020257e92785e2950a8f29b94065ef29ade8487
Emily Weston <emmylombardo@aol.com>
2023-03-08 11:16:21 -0500

    Updated Solution to LC 217

--- 217Leetcode.js
+++ 217Leetcode.js
@@ -22,14 +22,27 @@ const expectedOutput4 = true
 const test5 = [1, 2, 3, 1]
 const expectedOutput5 = true
 
-const containsDuplicate = (nums) => {
-    for(let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (i == j) continue
-            if(nums[i] == nums[j]) return true
+// original solution
+// const containsDuplicate = (nums) => {
+//     for(let i = 0; i < nums.length; i++) {
+//         for (let j = 1; j < nums.length; j++) {
+//             if (i == j) continue
+//             if(nums[i] == nums[j]) return true
+//         }
+//     }
+//     return false
+// };
+
+// updated solution
+var containsDuplicate = function(nums) {
+    for(let i = 0; i<nums.length; i++) {
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i]===nums[j]){
+                return true;
+            }
         }
     }
-    return false
+    return false;
 };
 
 console.log(containsDuplicate(test1))
